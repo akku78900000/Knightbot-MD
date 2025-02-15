882c2321752c73a66aa20ca0209429049a4f1196
akku78900000 <akkushark36@gmail.com>
2025-02-15 12:38:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Rgakku',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "RgAkku Bot",
+  botOwner: 'rgakku', // Your name
+  ownerNumber: '919947398456', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
